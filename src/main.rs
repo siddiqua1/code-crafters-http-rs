2582c8f050e9c94154259d3fa0b336a8f47a8ed0
Ash Siddiqui <ashsiddiqua00@gmail.com>
2024-03-18 20:52:12 -0400

    Stage 3 attempt 2

--- src/main.rs
+++ src/main.rs
@@ -25,8 +25,13 @@ const RESPONSE_OK: &[u8; 19] = b"HTTP/1.1 200 OK\r\n\r\n";
 const RESPONSE_404: &[u8; 26] = b"HTTP/1.1 404 Not Found\r\n\r\n";
 
 fn handle_valid_connection(stream: &mut TcpStream) {
-    println!("accepted new connection");
-    let response = parse_request(stream);
+    const MAX_HTTP_REQUEST_SIZE: usize = 8192;
+    let mut read_buffer: [u8; MAX_HTTP_REQUEST_SIZE] = [0; MAX_HTTP_REQUEST_SIZE];
+    // doing this really terribly on the first run
+    if let Err(_e) = stream.read(&mut read_buffer) {
+        println!("error: {}", _e);
+    }
+    let response = parse_request(&read_buffer);
     match stream.write(response) {
         Ok(_bytes_written) => {
             println!("{} bytes were written to the connection", _bytes_written);
@@ -37,13 +42,7 @@ fn handle_valid_connection(stream: &mut TcpStream) {
     }
 }
 
-fn parse_request(stream: &mut TcpStream) -> &[u8] {
-    const MAX_HTTP_REQUEST_SIZE: usize = 8192;
-    let mut read_buffer: [u8; MAX_HTTP_REQUEST_SIZE] = [0; MAX_HTTP_REQUEST_SIZE];
-    // doing this really terribly on the first run
-    if let Err(_e) = stream.read(&mut read_buffer) {
-        println!("error: {}", _e);
-    }
+fn parse_request(read_buffer: &[u8]) -> &[u8] {
     const VALID_REQUEST: &[u8; 16] = b"GET / HTTP/1.1\r\n";
     if &read_buffer[0..16] == VALID_REQUEST {
         return RESPONSE_OK;
